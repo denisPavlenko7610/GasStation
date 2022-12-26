852cd13bba7f44356e9fd5e3f9893c2e233c4626
Denis Pavlenko <denispavlenko7610new@gmail.com>
2022-12-26 17:13:23 +0200

    add test camera system

---
+++ Assets/Scripts/MonoBehaviours/CameraSingleton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace GasStation.MonoBehaviours
+{
+    public class CameraSingleton : MonoBehaviour
+    {
+       public static Camera Camerainstance { get; private set; }
+       public static Vector3 CameraOffset { get; private set; }
+
+        private void Awake()
+        {
+            Camerainstance = Camera.main;
+            CameraOffset = transform.position;
+        }
+    }
+}
